8b6a5767456ab052ad5d76fe1430295e7a55ab3a
oumondad <oumondad@e1r9p4.1337.ma>
2023-10-31 17:48:40 +0100

    first

---
+++ ft_atoi.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oumondad <oumondad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 14:51:08 by oumondad          #+#    #+#             */
+/*   Updated: 2023/10/31 15:38:49 by oumondad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	signe;
+	int	result;
+
+	i = 0;
+	signe = 1;
+	result = 0;
+	if (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			signe *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * signe);
+}
+/*
+int main()
+{
+	printf("%d",ft_atoi("-56769"));
+}
+*/
---
+++ ft_strchr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oumondad <oumondad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 16:59:07 by oumondad          #+#    #+#             */
+/*   Updated: 2023/10/31 17:40:40 by oumondad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*str;
+
+	str = (char *)s;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == (char)c)
+			return (&str[i]);
+		i++;
+	}
+	if (str[i] == (char)c)
+		return (&str[i]);
+	return (0);
+}
+/*
+int main()
+{
+	char str[] = "oussama";
+	printf("%s\n", ft_strchr(str, 'a'));
+	printf("%s\n", ft_strchr(str, 'x'));
+	printf("%s", ft_strchr(str, 0));
+}
+*/
