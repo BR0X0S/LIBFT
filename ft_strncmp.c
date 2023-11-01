7a4df77058278dc47d553e805a867dab1ead380f
oumondad <oumondad@e1r9p4.1337.ma>
2023-11-01 13:32:00 +0100

    add new function

---
+++ ft_strncmp.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oumondad <oumondad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 12:14:53 by oumondad          #+#    #+#             */
+/*   Updated: 2023/11/01 13:10:28 by oumondad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while ((str1[i] || str2[i]) && i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
+}
+/*
+int main()
+{
+	char str1[] = "oussama";
+	char str2[] = "oukassa";
+	int x;
+
+	x = ft_strncmp(str1, str2, 3);
+	printf("%d", x);
+}
+*/
